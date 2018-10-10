ae93e5146043aed6e6680f226adc58f904d00b91
Danijel Bjegovic <danijelbjegovic95@gmail.com>
2018-10-10 20:21:45 +0200

    User create, store, show, update and destroy with assigned roles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,4 +24,75 @@ class UserController extends Controller
             ->with('i', ($request->input('page', 1)-1)*5);
     }
 
+    public function create()
+    {
+        $permission = Permission::get();
+        return view('roles.create', compact('permission'));
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'      => 'required',
+            'email'     => 'required|email|unique:users,email',
+            'password'  => 'required|same:confirm-password',
+            'roles'     => 'required'
+        ]);
+
+        $input = $request->all();
+        $input['password'] = Hash::make($input['password']);
+
+        $user = User::create($input);
+
+        $user->assignRole($request->input('roles'));
+
+        return redirect()->route('users.index')
+            ->with('success', 'User created successfully');
+
+    }
+
+    public function edit($id)
+    {
+        $user = User::find($id);
+        $roles = Role::pluck('name', 'name')->all();
+        $userRole = $user->roles->pluck('name', 'name')->all();
+
+        return view('users.edit', compact('users', 'roles', 'userRole'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name'      => 'required',
+            'email'     => 'required|email|unique:users|email,'.$id,
+            'password'  => 'same:confirm-password',
+            'roles'     => 'required'
+        ]);
+        
+        $input = $request->all();
+
+        if(!empty($input['password'])){
+            $input['password'] = Hash::make($input['password']);
+        }else{
+            $input = array_except($input, array('password'));
+        }
+
+        $user = User::find($id);
+        $user->update($input);
+
+        DB::table('model_has_roles')->where('model_id', $id)->delete();
+
+        $user->assignRole($request->input('roles'));
+        return redirect()->route('users.index')
+            ->with('success', 'User updated successfully');
+    }
+
+    public function destroy($id)
+    {
+        User::find($id)->delete();
+        return redirect()->route('users.index')
+            ->with('success', 'User deleted successfully');
+
+    }
+
+
 }
